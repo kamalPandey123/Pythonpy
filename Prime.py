264d4e190adaa40ae857190559b2153fa3b60f03
kamalPandey123 <152475406+kamalPandey123@users.noreply.github.com>
2024-03-05 08:56:17 +0530

    Update Prime.py

--- Prime.py
+++ Prime.py
@@ -1,5 +1,12 @@
 num = int(input("Enter the number:"))
-if num%2 == 0:
-    print("Number is else")
+count = 0
+i =2
+for i in range(num -1):
+    if num % i == 0:
+        count += 1
+
+if count == 1:
+    print("Number is prime:")
 else:
-    print("Number is even:")
+   print("Number is Not prime:")    
+
